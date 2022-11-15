36e776fe17a49f25e3ec7cca6706e9253ccaedc2
Viki <hi@viki.moe>
2022-11-16 03:06:50 +0800

    feat: add config

---
+++ demo/index.ts
@@ -0,0 +1,22 @@
+import { createClient } from 'oicq'
+
+const account = 3252186223
+const client = createClient(account, { platform: 5 })
+
+client.on('system.online', () => client.logger.info('bot is online'))
+
+client.on('message', (e) => {
+  const isMaster = [1141284758].includes(e.sender.user_id)
+  const isGroup = e.message_type === 'group'
+  const isPrivateGroup = isGroup && [608391254].includes(e.group_id)
+
+  if (isMaster || isPrivateGroup) {
+    console.log(e)
+  }
+})
+
+const handlerQRcode = () => {
+  process.stdin.once('data', () => client.login())
+}
+
+client.on('system.login.qrcode', handlerQRcode).login()
