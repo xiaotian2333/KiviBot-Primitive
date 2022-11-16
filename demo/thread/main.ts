681c6c22216b4896bf1fa8e8f5a275bac5d7d966
Viki <hi@viki.moe>
2022-11-17 02:37:21 +0800

    chore: modify config

---
+++ demo/thread/main.ts
@@ -0,0 +1,28 @@
+import {
+  Worker,
+  MessageChannel,
+  markAsUntransferable,
+  isMainThread,
+  setEnvironmentData,
+  getEnvironmentData
+} from 'node:worker_threads'
+
+const { port1, port2 } = new MessageChannel()
+// const bc = new BroadcastChannel('hello')
+
+// if (isMainThread) {
+//   let c = 0
+//   bc.onmessage = (event) => {
+//     console.log(event.data)
+//     if (++c === 10) {
+//       bc.close()
+//     }
+//   }
+
+//   for (let n = 0; n < 10; n++) {
+//     const worker = new Worker(__filename)
+//   }
+// } else {
+//   bc.postMessage('hello from every worker')
+//   bc.close()
+// }
