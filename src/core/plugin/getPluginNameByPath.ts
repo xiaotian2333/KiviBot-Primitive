89b983656f673be5b1f39b6060c2d344a3b3307b
Viki <hi@viki.moe>
2022-11-25 22:04:21 +0800

    fix: 修复 windows 路径问题

--- src/core/plugin/getPluginNameByPath.ts
+++ src/core/plugin/getPluginNameByPath.ts
@@ -1,5 +1,9 @@
+import os from 'node:os'
+
+const isWin = os.platform() === 'win32'
+
 /** 通过模块路径获取插件名称，如果是 `npm` 插件，则自动去掉 `kivibot-plugin-` 前缀 */
 export function getPluginNameByPath(path: string) {
-  const paths = path.split('/')
+  const paths = path.split(isWin ? '\\' : '/')
   return paths[paths.length - 1].replace('kivibot-plugin-', '')
 }
