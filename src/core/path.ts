8e58f895803166c54d11bf14b6bcb036d74f6ca9
Viki <hi@viki.moe>
2023-01-04 13:52:20 +0800

    chore: add comment

--- src/core/path.ts
+++ src/core/path.ts
@@ -1,9 +1,16 @@
 import path from 'node:path'
 
+/** node 当前执行路径 */
 export const CWD = process.cwd()
+/** 配置文件路径 */
 export const ConfigPath = path.join(CWD, 'kivi.json')
+/** 框架 node_modules 路径 */
 export const NodeModulesDir = path.join(CWD, 'node_modules')
+/** oicq 数据目录 */
 export const OicqDataDir = path.join(CWD, 'data/oicq')
+/** logs 日志目录 */
 export const LogDir = path.join(CWD, 'logs')
+/** plugins 插件目录 */
 export const PluginDir = path.join(CWD, 'plugins')
+/** plugins 插件数据目录 */
 export const PluginDataDir = path.join(CWD, 'data/plugins')
