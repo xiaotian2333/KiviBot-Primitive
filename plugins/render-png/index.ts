b4f9df40e08b629f110eeee375c7c821aab61ab9
Viki <hi@viki.moe>
2023-08-29 08:52:59 +0800

    feat: add render test

--- plugins/render-png/index.ts
+++ plugins/render-png/index.ts
@@ -1,13 +1,31 @@
-import { useMount, setup } from '@kivi-dev/plugin'
+import { useMount, setup, useMatch, segment } from '@kivi-dev/plugin'
+import fs from 'node:fs'
+import path from 'node:path'
 import puppeteer from 'puppeteer'
 
 import { name, version } from './package.json'
 
+import type { MessageHandler } from '@kivi-dev/plugin'
+
 setup(name, version)
+
+const html = fs.readFileSync(path.join(__dirname, 'templates/daily-news.html'), 'utf-8')
 
 useMount(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+
+  async function renderHtml(html = '') {
+    await page.setContent(html)
+    await page.setViewport({ width: 640, height: 480, deviceScaleFactor: 1 })
+    return await page.screenshot()
+  }
+
+  const msgHandler: MessageHandler<'all'> = async (e) => {
+    e.reply(segment.image(await renderHtml(html)))
+  }
+
+  useMatch('/daily-news', msgHandler, { role: 'admin' })
 })
 
 export { plugin } from '@kivi-dev/plugin'
