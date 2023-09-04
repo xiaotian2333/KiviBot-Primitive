67d780c08498cfc9afb4454eb409f5e76dec85e8
Viki <hi@viki.moe>
2023-09-04 09:21:44 +0800

    chore: 更新示例插件

--- plugins/cron/index.ts
+++ plugins/cron/index.ts
@@ -1,13 +1,26 @@
-import { setup, useCron, useMount, bot, randomInt, wait } from '@kivi-dev/plugin'
+import {
+  bot,
+  defineCronHandler,
+  randomInt,
+  setup,
+  useCron,
+  useInfo,
+  useMount,
+  wait,
+} from '@kivi-dev/plugin'
 
 setup('cron', '1.0.0')
 
+const cronHandler = defineCronHandler(async () => {
+  const oneMinutes = 60 * 1000
+  const randomMinutes = randomInt(10 * oneMinutes, 20 * oneMinutes)
+
+  await wait(randomMinutes)
+  await bot().sendPrivateMsg(useInfo().mainAdmin, 'Hello world!')
+})
+
 useMount(() => {
-  useCron('7 * * * *', async () => {
-    const randomMinutes = randomInt(10 * 60 * 1000, 20 * 60 * 1000)
-    await wait(randomMinutes)
-    await bot().sendGroupMsg(123456, 'Hello world!')
-  })
+  useCron('7 * * * *', cronHandler)
 })
 
 export { plugin } from '@kivi-dev/plugin'
